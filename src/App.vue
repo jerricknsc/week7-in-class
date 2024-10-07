f696f48007586a070a6f86a86e6a0f35b7e2dd26
jerricknsc <jerricknsc@gmail.com>
2024-10-07 18:12:44 +0700

    hi

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
-<script setup>
+<!-- <script setup>
 import { onMounted } from 'vue';
 
 function beginParallex(){
-
-    let text = document.getElementById('text')
-    let btn = document.getElementById('letsGo')
-    let birds = document.getElementById('birds')
-
+    let bgImg = document.getElementById('background-img')
+    
     let scrollValue = window.scrollY
 
-    text.style.top = 40 + (scrollValue * -0.5) + '%'
-    btn.style.left = 45 + (scrollValue * -0.4) + '%'
-    birds.style.right = (scrollValue * -0.3) + '%'
+    // bgImg.style.scale = 1 + (scrollValue / 1000)
+    // if (bgImg.style.scale <= 2){
+
+    // }
+    console.log(bgImg.style.scale)
 }
 
 onMounted(() => {
@@ -21,74 +20,71 @@ onMounted(() => {
 </script>
 
 <template>
+    <div id="main-section">
+        <div id="background-img">
+            <img class="foreground" src="./images/file.png">
+            <img class="foreground" src="./images/mountain.png">
+        </div>
+    </div>
+
     <div id="paragraph">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit error accusantium optio dignissimos. Tenetur, architecto
             commodi ipsa asperiores voluptatum sequi aliquid quam incidunt accusamus impedit in! Ut magnam deserunt dignissimos?
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit error accusantium optio dignissimos. Tenetur, architecto
+            commodi ipsa asperiores voluptatum sequi aliquid quam incidunt accusamus impedit in! Ut magnam deserunt dignissimos?
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit error accusantium optio dignissimos. Tenetur, architecto
+            commodi ipsa asperiores voluptatum sequi aliquid quam incidunt accusamus impedit in! Ut magnam deserunt dignissimos?
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit error accusantium optio dignissimos. Tenetur, architecto
+            commodi ipsa asperiores voluptatum sequi aliquid quam incidunt accusamus impedit in! Ut magnam deserunt dignissimos?
     </div>
-    <section id="section">
-        <h1 id="text"> This is jerrick's app </h1>
-        <button id="letsGo"> Let's Go! </button>
-        <img id="birds" src="./images/birds.png">
-        <img id="snow-mountains" src="./images/snow-mountains.webp">
-    </section>
 
-    <body>
-        <div id="paragraph">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit error accusantium optio dignissimos. Tenetur, architecto
-            commodi ipsa asperiores voluptatum sequi aliquid quam incidunt accusamus impedit in! Ut magnam deserunt dignissimos?
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit error accusantium optio dignissimos. Tenetur, architecto
-            commodi ipsa asperiores voluptatum sequi aliquid quam incidunt accusamus impedit in! Ut magnam deserunt dignissimos?
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit error accusantium optio dignissimos. Tenetur, architecto
-            commodi ipsa asperiores voluptatum sequi aliquid quam incidunt accusamus impedit in! Ut magnam deserunt dignissimos?
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit error accusantium optio dignissimos. Tenetur, architecto
-            commodi ipsa asperiores voluptatum sequi aliquid quam incidunt accusamus impedit in! Ut magnam deserunt dignissimos?
+
+</template> -->
+
+
+<script setup>
+import { onMounted, onUnmounted } from 'vue';
+
+function beginParallax() {
+    let bgImg = document.getElementById('background-img');
+    let foregrounds = document.querySelectorAll('.foreground');
+
+    let scrollValue = window.scrollY;
+
+    // Parallax effect for background image
+    bgImg.style.transform = `translateY(${scrollValue * 0.5}px)`; // Slower scrolling for background
+
+    // Parallax effect for each foreground image
+    foregrounds.forEach((foreground, index) => {
+        // Each foreground layer scrolls at a slightly different speed for depth
+        foreground.style.transform = `translateY(${scrollValue * (0.3 - (index * 0.1))}px)`;
+    });
+}
+
+onMounted(() => {
+    window.addEventListener('scroll', beginParallax);
+});
+
+onUnmounted(() => {
+    window.removeEventListener('scroll', beginParallax); // Clean up event listener
+});
+</script>
+
+<template>
+    <div id="main-section">
+        <div id="background-img">
+            <!-- Foreground images for parallax effect -->
+            <img class="foreground" src="./images/file.png">
+            <img class="foreground" src="./images/mountain.png">
         </div>
-    </body>
+    </div>
+
+    <div id="paragraph">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit error accusantium optio dignissimos. Tenetur, architecto
+        commodi ipsa asperiores voluptatum sequi aliquid quam incidunt accusamus impedit in! Ut magnam deserunt dignissimos?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit error accusantium optio dignissimos. Tenetur, architecto
+        commodi ipsa asperiores voluptatum sequi aliquid quam incidunt accusamus impedit in! Ut magnam deserunt dignissimos?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit error accusantium optio dignissimos. Tenetur, architecto
+        commodi ipsa asperiores voluptatum sequi aliquid quam incidunt accusamus impedit in! Ut magnam deserunt dignissimos?
+    </div>
 </template>
-
-<style scoped>
-#section {
-    position: relative;
-    overflow-x: hidden;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-#text {
-    position: absolute;
-    z-index: 10;
-    font-size: 50px;
-    top: 40%;
-}
-
-#letsGo {
-    position: absolute;
-    z-index: 10;
-    top: 60%;
-    left: 45%;
-    padding: 15px;
-    background-color: antiquewhite;
-    border-radius: 10px;
-    min-width: 10rem;
-}
-
-#birds {
-    position: absolute;
-    z-index: 10;
-    scale: 0.4;
-    top: -40%;
-    right: 0;
-}
-
-#snow-mountains {
-    width: 100vw;
-    max-height: 80vh;
-    opacity: 0.7;
-}
-
-#paragraph {
-    font-size: 2rem;
-}
-
-</style>
