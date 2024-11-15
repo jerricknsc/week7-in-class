3501ee86269162e4e6165d390f4ef9da13a57999
jerricknsc <jerricknsc@gmail.com>
2024-11-15 12:01:58 +0800

    hi

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -1,119 +1,99 @@
 <script setup>
 import { ref } from 'vue';
+import draggable from 'vuedraggable';;
 
-// FOR FIREBASE
-import storage from '../../firebaseConfig';
-import { getDownloadURL, ref as storageRef, uploadBytes } from 'firebase/storage';
+const myArray = ref([
+    { name: 'jerrick', id: 0 },
+    { name: 'arin', id: 1 },
+    { name: 'shi hui', id: 2 },
+    { name: 'regan', id: 3 },
+])
 
-// FUNCTIONS TO MAKE API CALLS TO THE BACKEND
-import { createNewPost } from '@/services/PostService';
+const myArray2 = ref([
+    { name: 'hello', id: 0 },
+    { name: 'byebye', id: 1 },
+    { name: 'lousy', id: 2 },
+])
 
-// FORM INPUT VARIABLES
-var location = ref('')
-var image = ref('')
-var imageURL = ref('')
-var rating = ref('')
-var comment = ref('')
+const oldIndex = ref('')
+const newIndex = ref('')
 
-// SUCCESS OR FAILURE MSG
-var repsonseMsg = ref('')
-
-function handleImageUpload() {
-    image.value = event.target.files[0]
+function onEnd(event){
+    console.log(event)
 }
 
-async function handlePostBtnClicked() {
+const dragOptions = ref({
+    animation: 200,
+    group: 'description',
+    disabled: false,
+    ghostClass: 'ghost',
+});
 
-    // THIS UPLOADS THE IMAGE TO FIREBASE AND ASSIGNS THE IMAGE URL TO THE "imageURL" VARIABLE
-    if (image.value) {
-        let currentDate = new Date()
-        let imageStorageRef = storageRef(storage, `images/${image.value.name} ${currentDate}`)
-        let upload = await uploadBytes(imageStorageRef, image.value)
-        imageURL.value = await getDownloadURL(imageStorageRef)
+const message = [
+  'vue.draggable',
+  'draggable',
+  'component',
+  'for',
+  'vue.js 2.0',
+  'based',
+  'on',
+  'Sortablejs',
+];
 
-        let response = await createNewPost(location.value, imageURL.value, rating.value, comment.value)
+const list = ref(
+  message.map((name, index) => {
+    return { name, order: index + 1 };
+  })
+);
 
-        if (response.status === 200) {
-            repsonseMsg.value = 'Post uploaded successfully!'
-        }
-        else {
-            repsonseMsg.value = response
-        }
-    }
-
-    location.value = ''
-    image.value = ''
-    rating.value = ''
-    comment.value = ''
-
-}
 </script>
 
 <template>
-    <div class="container mt-5">
-        <div class="row mb-3">
-            <div class="col">
-                <label for="exampleFormControlInput1" class="form-label"> Enter location </label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Location"
-                    v-model="location">
-            </div>
-            <div class="col align-content-center">
-                <label for="exampleFormControlInput1" class="form-label"> Upload Photo </label>
-                <input type="file" class="form-control" accept="image/*" @change="handleImageUpload">
-            </div>
-        </div>
-        <div class="row mb-3 w-50">
-            <p class="mb-0"> Rating: </p>
-            <div class="form-check col ms-1">
-                <input class="form-check-input" type="radio" name="rating" value="5" id="fiveStars" checked
-                    v-model="rating">
-                <label class="form-check-label" for="fiveStars">
-                    5 stars
-                </label>
-            </div>
-            <div class="form-check col ms-1">
-                <input class="form-check-input" type="radio" name="rating" value="4" id="fourStars" v-model="rating">
-                <label class="form-check-label" for="fourStars">
-                    4 stars
-                </label>
-            </div>
-            <div class="form-check col ms-1">
-                <input class="form-check-input" type="radio" name="rating" value="3" id="threeStars" v-model="rating">
-                <label class="form-check-label" for="threeStars">
-                    3 stars
-                </label>
-            </div>
-            <div class="form-check col ms-1">
-                <input class="form-check-input" type="radio" name="rating" value="2" id="twoStars" v-model="rating">
-                <label class="form-check-label" for="twoStars">
-                    2 stars
-                </label>
-            </div>
-            <div class="form-check col ms-1">
-                <input class="form-check-input" type="radio" name="rating" value="1" id="oneStar" v-model="rating">
-                <label class="form-check-label" for="oneStar">
-                    1 star
-                </label>
-            </div>
-        </div>
+    <div>        
+        <h1> itinerary </h1>
 
-        <div class="row mb-3">
-            <div class="mb-3">
-                <label for="comments" class="form-label"> Comments </label>
-                <textarea class="form-control" id="comments" rows="3" placeholder="Enter comments here"
-                    v-model="comment"></textarea>
-            </div>
-        </div>
+        <transition-group>
+            <draggable v-model="myArray" item-key="id" key="draggable" v-bind="dragOptions" class="row">
+                <template #item="{element}">
+                    <div :key="element.id" class="draggable-item col-4">
+                        {{ element.name }}
+                    </div>
+                </template>
+            </draggable>
+        </transition-group>
 
-        <div class="row mb-3">
-            <div class="col">
-                <button type="button" class="btn btn-primary" @click="handlePostBtnClicked"> Post! </button>
-            </div>
-        </div>
+        
+        <br>
 
-        <div class='row'>
-            {{ repsonseMsg }}
-        </div>
+        <h1> shortlisted places </h1>
+
+        <transition-group>
+            <draggable item-key="name" key="dragggable" :list="list" v-bind="dragOptions">
+                <template #item="{ element }">
+                    <li :key="element.name" class="draggable-item2">
+                        {{ element.name }}
+                    </li>
+                </template>
+            </draggable>
+        </transition-group>
     </div>
-
 </template>
+
+<style>
+
+    .draggable-item {
+        padding: 8px;
+        margin: 4px 0;
+        background-color: #f0f0f0;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+    }
+
+    .draggable-item2 {
+        padding: 8px;
+        margin: 4px 0;
+        background-color: #ef9a9a;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+    }
+</style>
